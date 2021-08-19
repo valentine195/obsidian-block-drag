49bfd4ed52226febd44aae40682c1e1e66be8c79
valentine195 <38669521+valentine195@users.noreply.github.com>
2021-08-19 15:22:58 -0400

    implementation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
-import {
-    App,
-    MarkdownPostProcessorContext,
-    MarkdownSectionInformation,
-    MarkdownView,
-    Modal,
-    Notice,
-    Plugin,
-    PluginSettingTab,
-    Setting
-} from "obsidian";
+import { MarkdownPostProcessorContext, MarkdownView, Plugin } from "obsidian";
+
+import "./assets/styles.css";
 
 import Sortable from "sortablejs";
 
@@ -18,8 +10,7 @@ declare module "obsidian" {
     }
 }
 export default class BlockDragging extends Plugin {
-    map: Map<MarkdownView, Map<HTMLElement, MarkdownPostProcessorContext>> =
-        new Map();
+    map: Map<HTMLElement, MarkdownPostProcessorContext> = new Map();
     async onload() {
         console.log("Loading Block Dragging v" + this.manifest.version);
         await this.loadSettings();
@@ -30,88 +21,140 @@ export default class BlockDragging extends Plugin {
         this.registerEvent(
             this.app.workspace.on("active-leaf-change", (leaf) => {
                 if (!leaf.view || !(leaf.view instanceof MarkdownView)) return;
-                if (!leaf.view.file) return;
+                if (!this.app.workspace.getActiveFile()) return;
                 if (!leaf.view.containerEl) return;
+                const file = this.app.workspace.getActiveFile();
+                const { containerEl } = leaf.view;
 
-                if (!this.map.has(leaf.view)) {
-                    this.map.set(leaf.view, new Map());
+                const observer = new MutationObserver((list) => {
+                    for (const mutation of list) {
+                        if (
+                            mutation.type === "childList" &&
+                            mutation.removedNodes.length
+                        ) {
+                            this.map.delete(containerEl);
+                            observer.disconnect();
+                        }
+                    }
+                });
+
+                observer.observe(containerEl.parentElement, {
+                    childList: true,
+                    attributes: false,
+                    subtree: false
+                });
+
+                if (!this.map.has(containerEl)) {
+                    this.map.set(containerEl, undefined);
                 }
-                let contextMap = this.map.get(leaf.view);
-
-                const sizer = leaf.view.containerEl.querySelector(
+                const sizer = containerEl.querySelector(
                     ".markdown-preview-sizer"
                 );
                 if (!sizer || !(sizer instanceof HTMLElement)) return;
 
-                console.log(
-                    leaf.view.file,
-                    this.app.metadataCache.getFileCache(leaf.view.file).sections
-                );
-
                 let list: NodeListOf<HTMLDivElement> = sizer.querySelectorAll(
-                    "div:not(.collapse-indicator):not(:empty)"
+                    "div.block-drag-plugin-draggable-element"
                 );
-
                 const sort = new Sortable(sizer, {
-                    draggable: "div",
-                    filter: ".collapse-indicator, div > section.footnotes, :empty",
-
+                    draggable: "div.block-drag-plugin-draggable-element",
+                    filter: ".collapse-indicator",
+                    handle: ".block-drag-plugin-handle",
+                    delayOnTouchOnly: true,
+                    delay: 100,
+                    chosenClass: "block-drag-plugin-chosen",
+                    ghostClass: "block-drag-plugin-ghost",
                     onEnd: async (evt) => {
-                        list = sizer.querySelectorAll(
-                            "div:not(.collapse-indicator):not(:empty)"
-                        );
-                        let fileText = (
-                            await this.app.vault.cachedRead(leaf.view.file)
-                        ).split("\n");
-
-                        const myContext = contextMap.get(evt.item);
-                        const mySection = myContext.getSectionInfo(evt.item);
-                        const elementIAmNowBefore = list.item(
-                            evt.newDraggableIndex
+                        let removed = 0;
+                        let fileText = (await this.app.vault.read(file)).split(
+                            "\n"
                         );
 
-                        const newContext = contextMap.get(elementIAmNowBefore);
-                        const newSection =
-                            newContext.getSectionInfo(elementIAmNowBefore);
+                        const context = this.map.get(containerEl);
+                        const mySection = context.getSectionInfo(evt.item);
+
+                        //my text should also include my newlines
+                        const myNewLines = this.getNewlinesAfterIndex(
+                            fileText,
+                            mySection.lineEnd
+                        );
+
+                        const myStart = mySection.lineStart;
+                        const myEnd = mySection.lineEnd + myNewLines + 1;
+                        const myText = fileText.slice(myStart, myEnd);
 
                         fileText = [
-                            ...fileText.slice(0, mySection.lineStart),
-                            ...fileText.slice(
-                                mySection.lineEnd + 1,
-                                newSection.lineStart
-                            ),
-                            ...fileText.slice(
-                                mySection.lineStart,
-                                mySection.lineEnd + 1
-                            ),
-                            ...fileText.slice(newSection.lineStart)
+                            ...fileText.slice(0, myStart),
+                            ...fileText.slice(myEnd)
                         ];
 
-                        /* console.log(fileText); */
+                        /** The list will return the element before the dragged item if it goes forward. */
+                        const iWentForward = evt.newIndex > evt.oldIndex;
 
-                        await this.app.vault.modify(leaf.view.file, fileText.join('\n'))
+                        removed += (myEnd - myStart) * Number(iWentForward);
+
+                        const elementIAmNowBefore = list.item(
+                            evt.newDraggableIndex + Number(iWentForward)
+                        );
+                        const newSection =
+                            context.getSectionInfo(elementIAmNowBefore);
+
+                        if (!newSection) {
+                            fileText.push(...myText);
+                            await this.app.vault.modify(
+                                file,
+                                fileText.join("\n")
+                            );
+                            return;
+                        }
+
+                        fileText = [
+                            ...fileText.slice(
+                                0,
+                                newSection.lineStart - removed
+                            ),
+                            ...myText,
+                            ...fileText.slice(newSection.lineStart - removed)
+                        ];
+
+                        console.log(fileText);
+                        await this.app.vault.modify(file, fileText.join("\n"));
                     },
-                    onStart: (evt) => {
+                    onStart() {
                         list = sizer.querySelectorAll(
-                            "div:not(.collapse-indicator):not(:empty)"
+                            "div.block-drag-plugin-draggable-element"
                         );
                     }
                 });
-                console.log(
-                    "🚀 ~ file: main.ts ~ line 68 ~ sort",
-                    sort.toArray()
-                );
             })
         );
     }
+
+    getNewlinesAfterIndex(text: string[], index: number) {
+        const toSearch = text.slice(index + 1);
+        const findIndex = toSearch.findIndex((line) => line.length);
+
+        return findIndex < 0 ? toSearch?.length ?? 0 : findIndex;
+    }
+
     async postprocessor(el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+        if (!el.childElementCount) return;
+        if (el.querySelector("section.footnotes")) return;
+        if (el.querySelector(".frontmatter")) return;
+
         const view = this.app.workspace.getActiveViewOfType(MarkdownView);
         if (!view) return;
-        if (!this.map.has(view)) {
-            this.map.set(view, new Map());
+
+        if (
+            !this.map.has(view.containerEl) ||
+            this.map.get(view.containerEl) === undefined
+        ) {
+            this.map.set(view.containerEl, ctx);
         }
-        let contextMap = this.map.get(view);
-        contextMap.set(el, ctx);
+        el.addClass("block-drag-plugin-draggable-element");
+        el.createSpan({
+            cls: "block-drag-plugin-handle",
+            text: "⋮⋮"
+        });
     }
 
     onunload() {
